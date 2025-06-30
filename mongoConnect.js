2dd67a01f92f7e47256ca2e299b21e181260647a
codecraft-deb <debparna.dutta.2001@gmail.com>
2025-06-30 13:54:13 +0200

    Update mongoConnect.js

--- mongoConnect.js
+++ mongoConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect('mongodb+srv://<username>:i7ercptTueaKYxqz@cluster0.sbb6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://<username>:<put_your_password>@cluster0.sbb6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Connected to MongoDB successfully');
     }
     catch (error) {
